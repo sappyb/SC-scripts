34389a7332850357766bf4ad66e64ebbcee4f5df
Saptarshi Bhowmik <saptarshibhowmik@Saptarshis-MacBook-Air.local>
2020-04-13 14:41:01 -0400

    Fix indentation

--- dataExtraction.py
+++ dataExtraction.py
@@ -21,6 +21,9 @@ workload_3_list = []
 column_1 = []
 column_2 = []
 column_3 = []
+c_1 = 0
+c_2 = 0
+c_3 = 0
 for j in r:
     for k in s:
         for l in t:
@@ -46,23 +49,32 @@ for j in r:
                         fullList = []
                         fullList.append(fullDesc)
                         for pq, rs in zip(jobs, jobTime):
+                            if c_1 == 0:
+                                column_1.append(pq)
                             fullList.append(rs) 
+                        c_1 = 1
                         workload_1_list.append(fullList)
                     elif i == 'workload_2':
                         fullList = []
                         fullList.append(fullDesc)
                         for pq, rs in zip(jobs, jobTime):
+                            if c_2 == 0:
+                                column_2.append(pq)
                             fullList.append(rs) 
+                        c_2 = 1
                         workload_2_list.append(fullList)
                     elif i == 'workload_3':
                         fullList = []
                         fullList.append(fullDesc)
                         for pq, rs in zip(jobs, jobTime):
+                            if c_3 == 0:
+                                column_3.append(pq)
                             fullList.append(rs) 
+                        c_3 = 1
                         workload_3_list.append(fullList)
-df_1 = pd.DataFrame(workload_1_list)
-df_2 = pd.DataFrame(workload_2_list)
-df_3 = pd.DataFrame(workload_3_list)
+df_1 = pd.DataFrame(workload_1_list, columns = column_1)
+df_2 = pd.DataFrame(workload_2_list, columns = column_2)
+df_3 = pd.DataFrame(workload_3_list, columns = column_3)
 #print(df_1[0].str.split("/",expand=True))
 df_1[['i','p','lustre1','bhowmik1','Result','outputs','Topology','Mapping','Scheduling','Bandwidth','workload']] = df_1[0].apply( lambda x: pd.Series(str(x).split("/")))
 print(df_1)
